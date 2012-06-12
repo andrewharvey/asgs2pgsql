2da1e0b157f20b2c47da39d515afd11992d7a40f
Nyall Dawson <nyall@zombiepigs.net>
2012-06-12 18:47:05 +1000

    Escape strings in functions

--- stage2/98-load-extra-asgs-functions.sql
+++ stage2/98-load-extra-asgs-functions.sql
@@ -4,28 +4,28 @@
 -- as SA1 even if they are actually MB codes.
 CREATE OR REPLACE FUNCTION asgs_2011.match_abs_structure_code(code character varying(11)) RETURNS text AS $$
 BEGIN
-  IF code ~ '^\d$' THEN
+  IF code ~ E'^\\d$' THEN
       return 'state';
 
-  ELSIF code ~ '^\d{3}$' THEN
+  ELSIF code ~ E'^\\d{3}$' THEN
       return 'sa4';
 
   -- if 5 digits then is SA3
   -- UNLESS ends with 99 and 3rd last char is not a 9
-  ELSIF code ~ '^\d{5}$' THEN
+  ELSIF code ~ E'^\\d{5}$' THEN
     IF code ~ '[0-8]99$' THEN
       return 'gccsa';
     ELSE
       return 'sa3';
     END IF;
 
-  ELSIF code ~ '^\w{5}$' THEN
+  ELSIF code ~ E'^\\w{5}$' THEN
       return 'gccsa';
 
-  ELSIF code ~ '^\d{9}$' THEN
+  ELSIF code ~ E'^\\d{9}$' THEN
       return 'sa2';
 
-  ELSIF code ~ '^\d{11}$' THEN
+  ELSIF code ~ E'^\\d{11}$' THEN
       return 'sa1';
   END IF;
 END
@@ -34,14 +34,14 @@ $$ LANGUAGE plpgsql;
 -- For a given ASGS_2011.LGA name return a short version of the name
 CREATE OR REPLACE FUNCTION asgs_2011.shorten_lga_name(name text) RETURNS text AS $$
 BEGIN
-  return regexp_replace(name, ' \(.*', '');
+  return regexp_replace(name, E' \\(.*', '');
 END
 $$ LANGUAGE plpgsql;
 
 -- For a given ASGS_2011.LGA name return the LGA status type code
 CREATE OR REPLACE FUNCTION asgs_2011.lga_status_type_code(name text) RETURNS text AS $$
 BEGIN
-  return (regexp_matches(name, '\((.*)\)'))[1]::text;
+  return (regexp_matches(name, E'\\((.*)\\)'))[1]::text;
 END
 $$ LANGUAGE plpgsql;
 
