5e70261e083c2d79b1c35363c2e7df3f83d43db7
Andrew Harvey <andrew.harvey4@gmail.com>
2015-12-17 15:46:19 +1100

    add ASGS 2015 releases + fix grep

--- 00-make-download-code.sh
+++ 00-make-download-code.sh
@@ -9,7 +9,10 @@ for i in {1..6}
 do
   echo "# ASGS Volume $i"
   echo "# ABS Publication: 1270.0.55.00$i"
-  curl -o - "http://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.00${i}July%202011?OpenDocument" 2> /dev/null | grep --ignore-case --only-matching '/ausstats[^"]*' | grep -E '(Latest|Previous)$' | grep \.zip | grep --invert-match 'midmif\.zip' | cut -d'&' -f2,5,7,8,9 | sed 's/&/" "/g' | sed 's/$/"/' | sed "s/^127005500${i}_/download ${i} \"/"
-  echo ""
+  for year in 2011 2015
+  do
+      curl -o - "http://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.00${i}July%20${year}?OpenDocument" 2> /dev/null | grep --binary-files=text --ignore-case --only-matching '/ausstats[^"]*' | grep -E '(Latest|Previous)$' | grep \.zip | grep --invert-match 'midmif\.zip' | cut -d'&' -f2,5,7,8,9 | sed 's/&/" "/g' | sed 's/$/"/' | sed "s/^127005500${i}_/download ${i} \"/"
+    echo ""
+  done
 done
 
