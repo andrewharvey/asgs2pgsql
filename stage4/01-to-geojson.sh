b699b94e2a3e05134ff99573419f5382ae5beb79
Andrew Harvey <andrew.harvey4@gmail.com>
2017-01-04 13:13:17 +1100

    export to geojson

--- stage4/01-to-geojson.sh
+++ stage4/01-to-geojson.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-rm -rf output/geojson output/topojson
-mkdir -p output/geojson output/topojson
+rm -rf output/geojson
+mkdir -p output/geojson
 
-#for table in `psql --no-align --tuples-only --quiet -c "SELECT table_name FROM information_schema.tables WHERE table_schema = 'asgs_2011';"`; do
-for table in lga ; do
-    ogr2ogr -f GeoJSON -where "ste = '1'" output/geojson/asgs_2011.${table}.geojson PG:dbname=$dbname asgs_2011.$table
-    ~/node_modules/topojson/bin/topojson --quantization 80000 --simplify-proportion 0.5 --id-property code -o output/topojson/asgs_2011.${table}.topo.json output/geojson/asgs_2011.${table}.geojson
+for year in 2011 2015 2016 ; do
+    for table in `psql --no-align --tuples-only --quiet -c "SELECT table_name FROM information_schema.tables WHERE table_schema = 'asgs_${year}';"`; do
+        echo asgs_${year}.${table}
+        ogr2ogr -f GeoJSON -t_srs 'EPSG:4326' -lco COORDINATE_PRECISION=5 output/geojson/asgs_${year}.${table}.geojson PG:dbname=$dbname asgs_$year.$table
+        #pxz output/geojson/asgs_${year}.${table}.geojson
+    done
 done
