9f8ca5109e6b41a5cb0c58cb816032a5b4992818
Andrew Harvey <andrew.harvey4@gmail.com>
2018-04-10 13:19:09 +1000

    skip tab downloads

--- 00-make-download-code.sh
+++ 00-make-download-code.sh
@@ -11,7 +11,7 @@ do
   echo "# ABS Publication: 1270.0.55.00$i"
   for year in 2011 2012 2013 2014 2015 2016 2017 2018
   do
-      curl -o - "http://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.00${i}July%20${year}?OpenDocument" 2> /dev/null | grep --binary-files=text --ignore-case --only-matching '/ausstats[^"]*' | grep -E '(Latest|Previous)$' | grep \.zip | grep --invert-match 'midmif\.zip' | cut -d'&' -f2,5,7,8,9 | sed 's/&/" "/g' | sed 's/$/"/' | sed "s/^127005500${i}_/download ${i} \"/" | sed "s/^cg_/download cg \"/"
+      curl -o - "http://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.00${i}July%20${year}?OpenDocument" 2> /dev/null | grep --binary-files=text --ignore-case --only-matching '/ausstats[^"]*' | grep -E '(Latest|Previous)$' | grep \.zip | grep --invert-match 'midmif\.zip' | grep --invert-match 'tab\.zip' | cut -d'&' -f2,5,7,8,9 | sed 's/&/" "/g' | sed 's/$/"/' | sed "s/^127005500${i}_/download ${i} \"/" | sed "s/^cg_/download cg \"/"
     echo ""
   done
 done
